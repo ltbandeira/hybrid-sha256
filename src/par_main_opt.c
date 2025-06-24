6a6f545b0c6ba73a3654782d040359b7d62a1c4f
Lucas <lucastbandeira@gmail.com>
2025-06-24 15:28:58 -0300

    final

--- src/par_main_opt.c
+++ src/par_main_opt.c
@@ -48,9 +48,8 @@ void compute_sha256(const char *input, uint8_t output[SHA256_BLOCK_SIZE])
 
 int main(int argc, char **argv)
 {
-    int rank, size;
+    int rank, size, provided;
     double t_start, t_end;
-    int provided;
 
     MPI_Status status;
     MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
@@ -60,31 +59,64 @@ int main(int argc, char **argv)
     if (provided < MPI_THREAD_MULTIPLE)
     {
         if (rank == 0)
-            fprintf(stderr, "Erro: MPI não fornece MPI_THREAD_MULTIPLE (forneceu %d)\n", provided);
+            fprintf(stderr,
+                    "Erro: MPI não fornece MPI_THREAD_MULTIPLE (forneceu %d)\n",
+                    provided);
         MPI_Abort(MPI_COMM_WORLD, 1);
     }
 
-    t_start = MPI_Wtime();
-
+    /* -------- leitura dos argumentos -------- */
     if (argc < 4)
     {
         if (rank == 0)
-            fprintf(stderr, "Uso: mpirun -np <procs> %s <numero_solucoes> <bits_zero> <threads>\n", argv[0]);
+            fprintf(stderr,
+                    "Uso: mpirun -np <procs> %s <numero_solucoes> <bits_zero> "
+                    "<threads_openmp>\n",
+                    argv[0]);
         MPI_Finalize();
         return 1;
     }
-
     int total_solucoes = atoi(argv[1]);
     int target_zero_bits = atoi(argv[2]);
-    int num_threads = atoi(argv[3]);
-    omp_set_num_threads(num_threads);
+    int num_threads_cli = atoi(argv[3]);
+
+    /* -------- descoberta do hostname do mestre -------- */
+    char my_host[MPI_MAX_PROCESSOR_NAME];
+    char master_host[MPI_MAX_PROCESSOR_NAME];
+    int name_len;
+    MPI_Get_processor_name(my_host, &name_len);
+
+    if (rank == 0)
+        strcpy(master_host, my_host);
+
+    MPI_Bcast(master_host, MPI_MAX_PROCESSOR_NAME, MPI_CHAR,
+              0, MPI_COMM_WORLD);
+
+    /* -------- decide nº de threads OpenMP -------- */
+    int threads_ativas;
+
+    if (rank == 0)
+    {
+        threads_ativas = 1;
+    }
+    else
+    {
+        threads_ativas =
+            (strcmp(my_host, master_host) == 0)
+                ? num_threads_cli - 1
+                : num_threads_cli;
+        if (threads_ativas < 1)
+            threads_ativas = 1;
+    }
+    omp_set_num_threads(threads_ativas);
+
+    t_start = MPI_Wtime();
 
     if (rank == 0)
     {
         // Mestre
         unsigned long long next_start = 0;
-        int stop_signal = 1;
-        int found_count = 0;
+        int stop_signal = 1, found = 0;
 
         // Alocação de arrays para índice e string
         unsigned long long *indices = malloc(total_solucoes * sizeof *indices);
@@ -92,33 +124,32 @@ int main(int argc, char **argv)
         for (int i = 0; i < total_solucoes; i++)
             sols[i] = malloc(MAX_MSG_LEN);
 
-        // Loop de recepção
-        while (found_count < total_solucoes)
+        while (found < total_solucoes)
         {
             MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 
             if (status.MPI_TAG == TAG_PEDIDO)
             {
                 unsigned long long step;
-                MPI_Recv(&step, 1, MPI_UNSIGNED_LONG_LONG, status.MPI_SOURCE, TAG_PEDIDO, MPI_COMM_WORLD, &status);
-                MPI_Send(&next_start, 1, MPI_UNSIGNED_LONG_LONG, status.MPI_SOURCE, TAG_TRABALHO, MPI_COMM_WORLD);
+                MPI_Recv(&step, 1, MPI_UNSIGNED_LONG_LONG,
+                         status.MPI_SOURCE, TAG_PEDIDO, MPI_COMM_WORLD, &status);
+                MPI_Send(&next_start, 1, MPI_UNSIGNED_LONG_LONG,
+                         status.MPI_SOURCE, TAG_TRABALHO, MPI_COMM_WORLD);
                 next_start += step;
             }
             else if (status.MPI_TAG == TAG_SOLUCAO)
             {
-                // Recebemos 2 strings de 256 bytes: [0]=hash_str, [1]=índice como texto
                 char msg[2][MAX_MSG_LEN];
-                MPI_Recv(msg, 2 * MAX_MSG_LEN, MPI_CHAR, status.MPI_SOURCE, TAG_SOLUCAO, MPI_COMM_WORLD, &status);
+                MPI_Recv(msg, 2 * MAX_MSG_LEN, MPI_CHAR,
+                         status.MPI_SOURCE, TAG_SOLUCAO, MPI_COMM_WORLD, &status);
 
-                // Converte índice e armazena
-                indices[found_count] = strtoull(msg[1], NULL, 10);
-                strncpy(sols[found_count], msg[0], MAX_MSG_LEN - 1);
-                sols[found_count][MAX_MSG_LEN - 1] = '\0';
+                indices[found] = strtoull(msg[1], NULL, 10);
+                strncpy(sols[found], msg[0], MAX_MSG_LEN - 1);
+                sols[found][MAX_MSG_LEN - 1] = '\0';
 
-                printf("[MESTRE] recebido %2d) \"%s\" (idx=%llu) do worker %d\n",
-                       found_count + 1, sols[found_count], indices[found_count], status.MPI_SOURCE);
-
-                found_count++;
+                printf("[MESTRE] %2d) \"%s\" (idx=%llu) do worker %d\n",
+                       found + 1, sols[found], indices[found], status.MPI_SOURCE);
+                found++;
             }
         }
 
@@ -128,30 +159,21 @@ int main(int argc, char **argv)
 
         // Ordena resultados pelo índice
         for (int i = 0; i < total_solucoes - 1; i++)
-        {
             for (int j = i + 1; j < total_solucoes; j++)
-            {
                 if (indices[i] > indices[j])
                 {
-                    // Troca índices
                     unsigned long long tmp_idx = indices[i];
                     indices[i] = indices[j];
                     indices[j] = tmp_idx;
-                    // Troca strings
-                    char *tmp_str = sols[i];
+                    char *tmp = sols[i];
                     sols[i] = sols[j];
-                    sols[j] = tmp_str;
+                    sols[j] = tmp;
                 }
-            }
-        }
 
         printf("\n[MESTRE] Soluções ordenadas:\n");
         for (int i = 0; i < total_solucoes; i++)
-        {
             printf("  %2d) %s  (idx=%llu)\n", i + 1, sols[i], indices[i]);
-        }
 
-        // Libera memória
         for (int i = 0; i < total_solucoes; i++)
             free(sols[i]);
         free(sols);
@@ -159,21 +181,23 @@ int main(int argc, char **argv)
     }
     else
     {
-        // trabalhadores
+        // Trabalhadores
         int stop = 0;
+
         while (!stop)
         {
-            // Sondar se chegou sinal de parada ANTES de pedir trabalho
+            // Verifica parada antes de pedir trabalho
             int flag;
             MPI_Iprobe(0, TAG_PARADA, MPI_COMM_WORLD, &flag, &status);
             if (flag)
             {
                 int dummy;
-                MPI_Recv(&dummy, 1, MPI_INT, 0, TAG_PARADA, MPI_COMM_WORLD, &status);
+                MPI_Recv(&dummy, 1, MPI_INT, 0, TAG_PARADA,
+                         MPI_COMM_WORLD, &status);
                 break;
             }
 
-            // Pede trabalho
+            // Pede trabalho ao mestre
             unsigned long long step = STEP_SIZE;
             MPI_Send(&step, 1, MPI_UNSIGNED_LONG_LONG, 0, TAG_PEDIDO, MPI_COMM_WORLD);
 
@@ -181,45 +205,43 @@ int main(int argc, char **argv)
             while (1)
             {
                 MPI_Iprobe(0, MPI_ANY_TAG, MPI_COMM_WORLD, &flag, &status);
-                if (flag)
+                if (!flag)
+                    continue;
+
+                if (status.MPI_TAG == TAG_PARADA)
                 {
-                    if (status.MPI_TAG == TAG_PARADA)
-                    {
-                        int dummy;
-                        MPI_Recv(&dummy, 1, MPI_INT, 0, TAG_PARADA, MPI_COMM_WORLD, &status);
-                        stop = 1;
-                        break;
-                    }
-                    else if (status.MPI_TAG == TAG_TRABALHO)
-                    {
-                        unsigned long long start;
-                        MPI_Recv(&start, 1, MPI_UNSIGNED_LONG_LONG, 0, TAG_TRABALHO, MPI_COMM_WORLD, &status);
+                    int dummy;
+                    MPI_Recv(&dummy, 1, MPI_INT, 0, TAG_PARADA,
+                             MPI_COMM_WORLD, &status);
+                    stop = 1;
+                    break;
+                }
+                else if (status.MPI_TAG == TAG_TRABALHO)
+                {
+                    unsigned long long start;
+                    MPI_Recv(&start, 1, MPI_UNSIGNED_LONG_LONG, 0, TAG_TRABALHO, MPI_COMM_WORLD, &status);
 
-                        // Processa o trabalho recebido
-                        #pragma omp parallel for schedule(dynamic)
-                        for (unsigned long long i = start; i < start + step; i++)
+// Processa o trabalho recebido
+#pragma omp parallel for schedule(dynamic)
+                    for (unsigned long long i = start; i < start + step; i++)
+                    {
+                        uint8_t hash[SHA256_BLOCK_SIZE];
+                        char txt[1024];
+                        snprintf(txt, sizeof(txt), PREFIX "%llu", i);
+                        compute_sha256(txt, hash);
+
+                        // Prepara mensagem com índice e string
+                        if (hash_has_n_zero_bits(hash, target_zero_bits))
                         {
-                            char txt[1024];
-                            uint8_t hash[SHA256_BLOCK_SIZE];
-                            snprintf(txt, sizeof(txt), PREFIX "%llu", i);
-                            compute_sha256(txt, hash);
-
-                            if (hash_has_n_zero_bits(hash, target_zero_bits))
-                            {
-                                char hex_hash[SHA256_BLOCK_SIZE * 2 + 1];
-                                hash_to_hex(hash, hex_hash);
-
-                                // Prepara mensagem com índice e string
-                                char msg[2][MAX_MSG_LEN];
-                                snprintf(msg[0], MAX_MSG_LEN, "%s", txt);
-                                snprintf(msg[1], MAX_MSG_LEN, "%llu", i);
-                                int tid = omp_get_thread_num();
-                                printf("[Worker %d / thread %d] achou: %s \n", rank, tid, txt);
-                                MPI_Send(msg, 2 * MAX_MSG_LEN, MPI_CHAR, 0, TAG_SOLUCAO, MPI_COMM_WORLD);
-                            }
+                            char msg[2][MAX_MSG_LEN];
+                            snprintf(msg[0], MAX_MSG_LEN, "%s", txt);
+                            snprintf(msg[1], MAX_MSG_LEN, "%llu", i);
+                            int tid = omp_get_thread_num();
+                            printf("[Worker %d / thread %d] achou: %s \n", rank, tid, txt);
+                            MPI_Send(msg, 2 * MAX_MSG_LEN, MPI_CHAR, 0, TAG_SOLUCAO, MPI_COMM_WORLD);
                         }
-                        break;
                     }
+                    break;
                 }
             }
         }
